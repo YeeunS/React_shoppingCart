160600ebd89a723250812abd1aeb39c84d63a524
Yeeun Shin <yeeun2@hawaii.edu>
2024-05-20 18:09:55 -1000

    formatted properly

--- shoppingcart/src/APIs/api.js
+++ shoppingcart/src/APIs/api.js
@@ -2,19 +2,19 @@ const baseURL = "http://localhost:3000";
 
 // API functions
 export const getInventory = () => {
-    return fetch(`${baseURL}/inventory`).then((res) => res.json());
+  return fetch(`${baseURL}/inventory`).then((res) => res.json());
 };
 
 export const getCart = () => {
-    return fetch(`${baseURL}/cart`).then((res) => res.json());
+  return fetch(`${baseURL}/cart`).then((res) => res.json());
 };
 
 export const updateCart = (cart) => {
-    return fetch(`${baseURL}/cart`, {
-        method: "PUT",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(cart),
-    }).then((res) => res.json());
+  return fetch(`${baseURL}/cart`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(cart),
+  }).then((res) => res.json());
 };
